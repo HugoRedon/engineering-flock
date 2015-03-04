dbb72d66d94c5cca1e09addeefb9486f50cb735d
unknown <redonrih@REDONRIH2.corp.aspentech.com>
2015-03-03 18:30:09 -0600

    indentando archivo

--- src/EnergyToHeat.f
+++ src/EnergyToHeat.f
@@ -1,10 +1,10 @@
       program Energy
         double precision cE, result,numb
         double precision mass, cp,T2,T1
-	mass = 0.35d0
-	cp = 129d0
-	T2 = 50d0
-	T1 = 10d0
+		mass = 0.35d0
+		cp = 129d0
+		T2 = 50d0
+		T1 = 10d0
 	
         write(*,*) cE(mass,cp,T2,T1), logt(500,450,475,470)
         stop
--- src/LogTemp.f
+++ src/LogTemp.f
@@ -1,5 +1,5 @@
       double precision function logt(iT1,iT2,ot1,ot2)
-	double precision iT1,iT2,ot1,ot2
-	logt = ((iT1-ot2)-(iT2-ot1))/log((iT1-ot2)/(iT2-ot1))
+		double precision iT1,iT2,ot1,ot2
+		logt = ((iT1-ot2)-(iT2-ot1))/log((iT1-ot2)/(iT2-ot1))
       return
       end
